2fc97b17e3d6a79f51c04e86ab4af354b23a7304
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-04-13 09:50:05 -0700

    revert unrelated changes

--- scripts/makeDTSObj.mjs
+++ scripts/makeDTSObj.mjs
@@ -1,8 +1,8 @@
 import { recursiveReadDirSync } from "./recursiveReadDirSync.mjs"
 import {readFileSync, writeFileSync} from "fs"
 
+/** @type {string[]} */
 const dtsFiles = recursiveReadDirSync("./node_modules/@octokit").filter(f => f.endsWith(".d.ts") || f.endsWith(".json") || f.endsWith(".js"))
 const dtsObj = {}
 dtsFiles.forEach(key => dtsObj[key] = readFileSync(key, "utf8"))
 writeFileSync("vendor/dts.js", `const DTS = ${JSON.stringify(dtsObj)}; export default DTS`)
-console.log("vendor/dts.js written")
